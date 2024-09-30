0d35836cd67578d5e58c98bfa3ab6b2288305b69
PberuKatwa <pberu8531@gmail.com>
2024-09-30 10:56:48 +0300

    first-commit

--- node-backend/userData/config/database.js
+++
@@ -1,5 +0,0 @@
-const Sequelize = require('sequelize');
-module.exports = new Sequelize('node_database', 'postgres', 'postgres', {
- host: 'localhost',
- dialect: 'postgres', // Change to your database type
-});
